525abf368a5e9f7337adb755b203490df4a0269e
Pham Huy Ha <ha.phamhuy@vietis.vn>
2019-11-06 17:35:27 +0700

    282 Adding a Drop-down Menu

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCakWPvOW1Evj7i6XC3IvJusHs0dVI5MJA",
+    authDomain: "midori-shop.firebaseapp.com",
+    databaseURL: "https://midori-shop.firebaseio.com",
+    projectId: "midori-shop",
+    storageBucket: "midori-shop.appspot.com",
+    messagingSenderId: "660681891393",
+    appId: "1:660681891393:web:295c29d1f9928fb1e370c0",
+    measurementId: "G-CP1374QHFC"
+  }
+};
