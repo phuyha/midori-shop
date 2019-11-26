3286967d17f7caa01ea7f0fcb1766ae3170a1688
Pham Huy Ha <ha.phamhuy@vietis.vn>
2019-11-26 18:57:51 +0700

    Still error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ import { DataTableModule } from 'angular7-data-table';
     CustomFormsModule,
     DataTableModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: ProductsComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories$;
+  category: string;
 
-  constructor() { }
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService
+  ) {
+    productService.getAll().subscribe(products => this.products = products);
+    this.categories$ = categoryService.getAll();
 
-  ngOnInit() {
+    route.queryParamMap.pipe().subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) : this.products;
+    });
   }
 
 }
